2b5ccfab48d6042dac1b00f83f3cdbfb6fae6fc3
Belén Montaluisa <belenmont19@gmail.com>
2025-11-24 10:28:03 -0500

    Script para descarga de secuencias del genoma de NCBI
    Updated comments for clarity and consistency in the script.

--- Scripts/Script_genoma.sh
+++ Scripts/Script_genoma.sh
@@ -3,11 +3,10 @@
 
 URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/019/023/105/GCA_019023105.1_LSU_DiBr_2.0/"
 
-# Se obtiene el filtrado de archivos con grep
-## -E permite usar expresiones regulares extendidas (para buscar patrones más complejos) y -o extrae sólo la parte del texto que coincide con el patrón. 
-## 'href="[^"]+\.gz"' 
-## se utiliza pipe | para agregar cut -d "', que corta usando como delimitador (") y con -f2 se queda con el segundo campo. 
-## Todos los nombres de los archivos se guardan en la lista de archivos de cromosomas.
+# Se obtiene el filtrado de archivos con grep -E permite usar expresiones regulares extendidas (para buscar patrones más complejos) y -o extrae sólo la parte del texto 
+# que coincide con el patrón. 'href="[^"]+\.gz"' 
+# se agrega cut -d "', que corta usando como delimitador (") y con -f2 se queda con el segundo campo. 
+# Todos los nombres de los archivos se guardan en la lista de archivos de cromosomas.
 
 curl -s $URL | grep -Eo 'href="[^"]+\.gz"' | cut -d'"' -f2 > lista_archivos_cromosomas.txt
 
