659e5c682ac01772c9857a4dbb092062d0a5efcd
Belén Montaluisa <belenmont19@gmail.com>
2025-11-24 10:19:48 -0500

    Modify URL for genome download in Script_cromosoma.sh
    Updated URL variable for chromosome data download.

--- Scripts/Script_cromosoma.sh
+++ Scripts/Script_cromosoma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/019/023/105/GCA_019023105.1_LSU_DiBr_2.0/GCA_019023105.1_LSU_DiBr_2.0_assembly_structure/Primary_Assembly/assembled_chromosomes/FASTA/"
-
+# La variable URL se cambiará dependiendo del genoma del ave de interés. 
 # Obtener listado de archivos
 curl -s $URL | grep -Eo 'href="[^"]+\.gz"' | cut -d'"' -f2 > lista_archivos.txt
 
